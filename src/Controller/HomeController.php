299699458ffb4dc9263c46e3e2f2cf3e1f88ee6a
Marcin Puszka <marcin.puszka@iamdripfeed.com>
2020-08-03 11:17:15 +0200

    setting up project and functionalites

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use GuzzleHttp\Client;
+
+class HomeController extends AbstractController
+{
+    /**
+     * Constans variable for cache expiration in seconds
+     */
+    public const CACHE_EXPIRATION = 60;
+
+    /**
+     * Home method for route /
+     *
+     * @return object
+     */
+    public function index(): object
+    {   
+        $cache      = new FilesystemAdapter();
+        $cacheData  = $cache->getItem('guzzle.data');
+
+        if (!$cacheData->isHit()) {
+            $client         = new Client();
+            $response       = $client->get('https://jsonplaceholder.typicode.com/comments');
+            $responseCode   = $response->getStatusCode();
+            $comments       = [];
+
+            if (200 !== $responseCode) {
+                throw $this->createNotFoundException('Something goes wrong with data!');
+            }
+
+            $body = $response->getBody();
+            if (true === empty($body)) {
+                $this->addFlash(
+                    'error',
+                    'Empty body!'
+                );
+            } else {
+                $comments = json_decode($body->getContents(), true);
+            }
+
+            $cacheData->set($comments);
+            $cacheData->expiresAfter(self::CACHE_EXPIRATION); 
+
+            $cache->save($cacheData);
+        }
+
+        $comments = $cache->getItem('guzzle.data')
+                        ->get();
+ 
+        return $this->render('home/index.html.twig', [
+            'comments'          => $comments
+        ]);
+    }
+}
